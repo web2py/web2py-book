702b8daf9017e009b956b391dee62b2183fb7e02
mdipierro <massimo.dipierro@gmail.com>
2012-12-27 17:04:13 -0600

    fixed \@/ in pdf

--- private/convert_book.py
+++ private/convert_book.py
@@ -171,6 +171,7 @@ def assemble(path):
         reference = getreference(os.path.join(path,'references',item))
         bibliography.append((item,reference['source_url'])) 
     txtitems = '\n'.join('\\bibitem{%s} \\url{%s}' % item for item in bibliography)
+    body = body.place('\@/','@/')
     return HEADER + body + FOOTER.replace('@BIBITEMS',txtitems)
 
 if __name__=='__main__':
